cef2b9b6c818df9ec02db62ecb9da31a9e44b838
Jeremy Soller <jackpot51@gmail.com>
2024-01-19 21:13:11 -0700

    zbus implementation

---
+++ generate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+rm -rfv src
+mkdir -p src
+pushd src
+
+cargo run --manifest-path ../zbus/zbus_xmlgen/Cargo.toml --release -- \
+    ../PackageKit/src/org.freedesktop.PackageKit.xml
+cargo run --manifest-path ../zbus/zbus_xmlgen/Cargo.toml --release -- \
+    ../PackageKit/src/org.freedesktop.PackageKit.Transaction.xml
+
+for file in *.rs
+do
+    name="$(basename $file .rs)"
+    echo "pub use ${name}::*;" >> lib.rs
+    echo "mod ${name};" >> lib.rs
+done
+echo "pub use zbus;" >> lib.rs
+
+popd
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub use Offline::*;
+mod Offline;
+pub use PackageKit::*;
+mod PackageKit;
+pub use Transaction::*;
+mod Transaction;
+pub use zbus;
