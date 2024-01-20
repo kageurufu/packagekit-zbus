dcc3cb9d2320d74b47beec2f70cfa261e178cdbe
Jeremy Soller <jackpot51@gmail.com>
2024-01-19 20:52:58 -0700

    Delete packagekit-glib code

--- README.md
+++
@@ -1,4 +0,0 @@
-# packagekit-rs
-Rust bindings for PackageKit
-
-You must install `libpackagekit-glib2-dev` and `rustdoc-stripper` to generate
--- packagekit/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub use ffi;
-pub use gio;
-pub use glib;
-pub use self::auto::*;
-pub use self::functions::*;
-pub use self::traits::*;
-mod auto;
